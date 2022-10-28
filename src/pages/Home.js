60710b41d21bdbf2354a6bd3a0ee6a383bff783e
Blaine Van Item <blainevanitem2000@gmail.com>
2022-10-28 22:30:07 +0000

    Finished home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
 const Home = () => {
   return (
       <div class="homepage">
-      <center><h1>Home</h1>
-      
+      <center>
+      <h1>Welcome to Go Green!</h1>
+      <hr/>
+      <div class="row" id="container">
+        <div class="col">
+          <div class="row w-100">        
+            <img id="homeimage" class="d-inline-block" src="images/dock_lake.jpg"/>
+          </div>
+          <div  id="homep"class="row w-100">
+            <hr/>
+            <p><em>"This is the moment when we must come together to save this planet. Let us resolve that we will not leave our children a world where the oceans rise and famine spreads and terrible storms devastate our lands."</em> - Barack Obama</p>
+            <hr/>
+            <p><em>"One can see from space how the human race has changed the Earth. Nearly all of the available land has been cleared of forest and is now used for agriculture or urban development. The polar icecaps are shrinking and the desert areas are increasing. At night, the Earth is no longer dark, but large areas are lit up. All of this is evidence that human exploitation of the planet is reaching a critical limit. But human demands and expectations are ever-increasing. We cannot continue to pollute the atmosphere, poison the ocean and exhaust the land. There isn't any more available."</em> - Stephen Hawking</p>
+            <hr/>
+            <p><em>"If we take all this actions and if it turns out not be true, we have reduced pollution and have better ways to live, the downside is very small. The other way around, and we don’t act, and it turns out to be true, then we have betrayed future generations and we don’t have the right to do that."</em> - Tony Blair</p>
+            <hr/>
+          </div>
+        </div>
+        <div class="col">
+          <div  id="homex" class="row w-100">
+            <center><h3>The Earth is Our Home</h3></center>
+            <p id="homey">&emsp;Here at Go Green! our mission is to raise awareness and help those who do not know how to reduce their 
+            effect on the world start doing so! We take pride in the world around us and want to make sure that all
+            people unite to save the earth.</p>
+            <p id="homey">&emsp;In the Go Green community we have cerified ecologists, biologists and social leaders that
+            help give us an upper hand in what we can do personally to change the eventual outcome of the Earth and how we 
+            can present the information to the world. To contact us or learn more about us follow the links at the top of the page!
+            </p>
+            <p id="homey3">&emsp;We hope you come join us at our monthly events and learn how you can make a difference!
+            </p>
+          </div>
+          <div class="row w-100">           
+            <img id="homeimage" class="d-inline-block" src="images/windmill.jpg"/>
+          </div>
+        </div>
+      </div>
       </center>
+      <footer><center>Github: https://github.com/bvanitem/creative-project-3 Contributers: Blaine Van Item & Ameilya Monson</center></footer>
       </div>
       );
 };
