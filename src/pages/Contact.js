ba20def67364a434950af132bdd6de46b47cb532
Blaine Van Item <blainevanitem2000@gmail.com>
2022-10-29 21:32:34 +0000

    Contact us Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,22 @@
 const Contact = () => {
   return (
       <div class="contactpage">
-      <center><h1>Contact</h1>
-      
+      <center>      <h1>Contact Us!</h1>
+      <hr/>
+      <div class="row" id="container">
+        <div class="col"></div>
+        <div id="contactcont" class="col">
+                    <img id="homeimage" class="d-inline-block w-100" src="images/polution.jpg"/>
+              <h3>Contact us to start making a difference!</h3>
+              <hr/>
+            <p>At our office: 123 Go Green Way, Provo, Utah</p>
+            <p>Via Email: gogreen@gmail.com</p>
+            <p>Call us: (123) 456-7899</p>
+            </div>
+        <div class="col"></div>
+      </div>
       </center>
+      <footer><center>Github: https://github.com/bvanitem/creative-project-3 Contributers: Blaine Van Item & Ameilya Monson</center></footer>
       </div>
       );
 };
