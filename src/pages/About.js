ccc193806115eaeb5269a52ebff844b2462eb604
Ameilya Monson <ameilyamonson@gmail.com>
2022-10-29 04:08:17 +0000

    worked on about page and added corresponding css to index.css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,32 @@
 const About = () => {
   return (
-      <div class="aboutpage">
-      <center><h1>About Us</h1>
-      
-      </center>
+    <div class="aboutpage">
+      <div>
+        <h1>Our Mission</h1>
+        <h2>Our mission here at Go Green is to raise awareness and help those who do not know how to reduce their 
+            effect on the world start doing so! We take pride in the world around us and want to make sure that all
+            people unite to save the earth.</h2>
       </div>
-      );
+       <img id="aboutimage" class="d-inline-block" src="images/forest.jpg"/>
+      <div>
+        <h1>Why Go Green</h1>
+        <p>Our community is full of environmental diversity and natural beauty, but unfortunately as the years have passed, our community has started to become
+         bogged down. While part of the problem is ignorance, we know that there are so many people who want to make a difference.
+         At Go Green we have cerified ecologists, biologists and social leaders that help us provide education and meaningful opportunities for
+         people to get involved, and do their part to save the earth.</p>
+      </div>
+      <div>
+        <h1>Core Values</h1>
+        <ul>
+          <li>build a positive future</li>
+          <li>sustainable, simple, community-led projects</li>
+          <li>offer simple ways to make a change</li>
+          <li>provide easily-accessible education on environmental issues</li>
+        </ul>
+      </div>
+      <footer><center>Github: https://github.com/bvanitem/creative-project-3 Contributers: Blaine Van Item & Ameilya Monson</center></footer>
+    </div>
+  );
 };
 
 export default About;
